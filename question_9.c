a06538a38b8591bc559a4e9123b9e2a499568d1b
Abhishek kumar <113863385+abhi6753@users.noreply.github.com>
2025-03-31 19:48:51 +0530

    Update question_9.c

--- question_9.c
+++ question_9.c
@@ -1,16 +1,33 @@
 //9. Write a program to check whether a given number is an Armstrong number or not
 #include<stdio.h>
+#include<math.h>
 #include<conio.h>
-int main()
-{
-    int num,temp_num,rem,sum=0;
-    printf("Enter number to check armstrong or not:\n");
+//using namespace std;
+ int main()
+ {
+    
+    int num,sum=0,temp_num;
+    short countDigit=0,rem;
+    printf("Enter +ve integer to check is an Armstrong number or not: \n");
     scanf("%d",&num);
+    if(num<=0)
+    {
+        printf("Error! Plz. enter +ve integer\n");
+        return 0;
+    }
+    temp_num=num;
+    while (temp_num)// this condition is used to count digit
+    {
+       countDigit++;
+        temp_num/=10;
+       
+    }
     temp_num = num;
-    while(temp_num)
+    while (temp_num) // this condition is used to check number is armstrong or not
     {
         rem = temp_num%10;
-        sum +=rem*rem*rem; 
+        power = 
+        sum = sum + floor(pow(rem,countDigit));
         temp_num/=10;
     }
     if(num == sum)
